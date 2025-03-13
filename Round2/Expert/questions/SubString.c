11b2aa1fc8bf9712c032ec0f9485731191da28ac
MakandarSahil <sahilmakandar15@gmail.com>
2025-03-13 21:14:50 +0530

    diksha OP

--- Round2/Expert/questions/SubString.c
+++ Round2/Expert/questions/SubString.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+//program to find index of substring in a string 
 int findSubstring(char str, char sub)
 {
     int strLen = strlen(str);
     int subLen = strlen(sub);
-    if (subLen < strLen)
+    if (subLen > strLen)
     {
         return -1;
     }
     for (int i = 0; i < strLen - subLen - 1; i++)
     {
-        if (strncmp(str, sub, subLen) != 0)
+        if (strcpy(str, sub, subLen) != 0)
         {
             return i;
         }
@@ -25,11 +26,11 @@ int main()
     char str[] = "AI will take your job and you will remain jobless if you can't solve this problem on your own, ohhh sorry, you don't even have a job to lose :)";
     char sub[] = "this";
 
-    printf("%d\n", strlen(str));
+    printf("%d\n", str.length());
     int index = findSubstring(sub, str);
     if (index != -1)
     {
-        printf("%c\n", index);
+        printf("%c\n", str);
     }
     else
     {
