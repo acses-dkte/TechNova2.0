2954c53cab59ab41ea7599d05ed9e68a38258ee3
MakandarSahil <sahilmakandar15@gmail.com>
2025-03-14 13:02:53 +0530

    mai nahi bataunga

--- Round3/Novice/hard/Independence.c
+++ Round3/Novice/hard/Independence.c
@@ -1,3 +1,28 @@
 /*
   Link yaha hai - https://www.codechef.com/problems/INDEPENDENCE?tab=statement
 */
+
+#include <stdio.h>
+const char* flag(int o, int w, int g){
+    int max_flag = o;
+    if(max_flag < w){max_flag = w;}
+    if(max_flag < g){max_flag = g;}
+    
+    int other_flag = o + w + g - max_flag;
+    if(max_flag > other_flag + 1){
+        return "NO";
+    }else{
+        return "YES";
+    }
+    
+}
+int main(void) {
+	// your code goes here
+	int t,o,w,g;
+	scanf("%d",&t);
+    while(t--){
+        scanf("%d %d %d",&o,&w,&g);
+        printf("%s\n" , flag(o,w,g));
+    }
+}
+
